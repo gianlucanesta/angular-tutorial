d9907b224033567165b714cd442a26d9b56340dd
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-10-23 22:36:39 +0200

    Components Lifecycle

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
-  styleUrls: ['./prova.component.css']
+  styleUrls: ['./prova.component.css'],
 })
-export class ProvaComponent {
-
+export class ProvaComponent
+  implements
+    OnInit,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    DoCheck,
+    OnDestroy
+{
+  constructor() {
+    console.log('costruttore');
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 }
