f954feed1511f429ea6db2c98c9961bf9393ba41
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-10-29 18:37:54 +0100

    Template Variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-tutorial';
 
   persone = [
@@ -14,33 +20,16 @@ export class AppComponent {
     { nome: 'anna', cognome: 'neri', isOnline: false, color: 'orange' },
   ];
 
-  btnClick() {
-    this.persone = [
-      { nome: 'gianluca', cognome: 'rossi', isOnline: true, color: 'blue' },
-      { nome: 'gianmarco', cognome: 'verdi', isOnline: true, color: 'red' },
-      { nome: 'gianpiero', cognome: 'neri', isOnline: false, color: 'orange' },
-    ];
+  valore = 'ciao';
+
+  @ViewChild('inputSaluti') inputSaluti!: ElementRef<HTMLInputElement>;
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    console.log(this.inputSaluti);
   }
 
-  isVisible = true;
-  // numero = 3;
-  stringa = 'topolino';
-
-  color = 'orange';
-
-  onInput(event: Event) {
-    this.title = (<HTMLInputElement>event.target).value;
-  }
-
-  onClick(event: Event) {
-    this.title = 'ho cliccato sul bottone';
-  }
-
-  onRiceviDati(value: string) {
-    alert(value);
-  }
-
-  getColor() {
-    return 'green';
+  onClick() {
+    console.log(this.inputSaluti.nativeElement.value);
   }
 }
