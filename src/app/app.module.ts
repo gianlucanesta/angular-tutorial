140465c18909e1c5068415b2c84fbbf86b55ae54
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-10-31 18:14:06 +0100

    Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HighlightDirective } from './direttive/highlight/highlight.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -34,6 +34,7 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { interval } from 'rxjs';
 
 @Component({
@@ -8,20 +8,28 @@ import { interval } from 'rxjs';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  @ViewChild('homeform') homeform!: NgForm;
+  homeform!: FormGroup;
 
   sottoscrizione: any;
   constructor() {}
 
   ngOnInit(): void {
+    // Subscriber
     this.sottoscrizione = interval(1000).subscribe((numero) => {
       // console.log(numero);
+    });
+
+    // Reactive Form
+    this.homeform = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      colore: new FormControl(),
     });
   }
   ngOnDestroy(): void {
     this.sottoscrizione.unsubscribe();
   }
-  onSubmit(form: NgForm): void {
+  onSubmit(): void {
     console.log(this.homeform);
   }
 }
